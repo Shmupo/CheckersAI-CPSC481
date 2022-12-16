9a9c304dee004333ce8996d80a8f09ff6238556f
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-12-15 17:10:10 -0800

    Update extrafile.py

--- extrafile.py
+++ extrafile.py
@@ -5,23 +5,21 @@ from copy import deepcopy
 def minmax_alpha_beta(position, depth, max_player,game):
     if depth == 0 or game.check_win():
         return position
-    best_move = 0
+    
     if max_player:
+        best_move = None
         alpha = -inf
         for move in get_all_pieces(position, game):
             new_state = get_move(position, move)
             eval = minmax_alpha_beta(new_state, depth - 1,False,game)[0]
-            print(eval)
             print(f"Type of alpha is {type(alpha)} and type of val is {type(eval)}")
-           # best_score = min(best_score, score)
-          #  beta = min(beta, best_score)
             best_move = max(best_move, new_state)
             val = max(alpha, eval)
-            #val = self.minmax(move, depth-1, True , self.checkers)[0]
-            if beta <= alpha:
-                break
+            if val <= alpha:
+                best_move = move
         return best_move
     else:
+        best_move = None
         beta = inf
         for move in get_all_pieces(position,game):
             new_state = get_move(position, move)
@@ -29,8 +27,16 @@ def minmax_alpha_beta(position, depth, max_player,game):
             best_move = min(best_move, new_state)
             val = min(beta, eval)
             if beta <= val:
-                break
+                best_move = move
         return best_move
+
+def print_board(self):
+        print('---Game Board---')
+        for row in self.game_board:
+            for element in row:
+                print(element, end ='')
+            print()
+        print('----------------')
 
 def get_move(piece,move,tempBoard):
     tempBoard.make_move(piece, move,tempBoard)
@@ -46,7 +52,7 @@ def get_all_moves(board,color,game):
             temp_piece = temp_board.get_piece(piece[0],piece[1])
             new_board = get_move(temp_piece,currMove,temp_board)
             print(f"Temp piece {type(temp_piece)} and new_board {type(new_board)}")
-            moves.append([new_board,piece])
+            moves.append(new_board)
     return moves
     
 
@@ -62,3 +68,4 @@ def get_all_pieces(board, color):
                 valid_moves[piece] = board.moves_of_piece(piece)
     print(valid_moves)
     return valid_moves
+
