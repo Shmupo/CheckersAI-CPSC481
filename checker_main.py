845d8d9021000cad4b620cd21c7d5a7ea77f1cc0
Shmupo <andrewdoan8b@gmail.com>
2022-12-15 21:26:15 -0800

    Update checker_main.py

--- checker_main.py
+++ checker_main.py
@@ -1,3 +1,19 @@
+#==============================================
+# Filename: checker_main.py
+#==============================================
+# Filetype: Python Source File
+#==============================================
+# Author(s):
+#               Angaar Hamid
+#               Andrew Doan
+#               Alejandro Ramos
+#==============================================
+# Last Modified: 12/15/2022
+#==============================================
+# Description:
+# This file is responsible for 
+#==============================================
+
 import pygame
 from constants import WIDTH, HEIGHT
 from constants import CELL_SIZE
@@ -10,11 +26,34 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
+
+#==============================================
+# Function Name: get_row_col_from_mouse
+#==============================================
+# Description: returns the the row and column 
+# of a square the mouse is hovering over.
+#==============================================
+# Input: position of the mouse.
+#==============================================
+# Output: position of a square on the checkers
+# board.
+#==============================================
 def get_row_col_from_mouse(pos):
     x, y = pos
     row = y // CELL_SIZE
     col = x // CELL_SIZE
     return row, col
+
+#==============================================
+# Function Name: main
+#==============================================
+# Description: Initiates the checkers game and AI
+# and runs the main game loop.
+#==============================================
+# Input: None
+#==============================================
+# Output: None
+#==============================================
 
 def main():
     run = True
