2b3e71a512411499a3c04375896a7d12ff1c5502
Shmupo <andrewdoan8b@gmail.com>
2022-12-15 21:22:55 -0800

    Update checker_game.py

--- checker_game.py
+++ checker_game.py
@@ -1,33 +1,74 @@
-import pygame
+#==============================================
+# Filename: checker_game.py
+#==============================================
+# Filetype: Python Source File
+#==============================================
+# Author(s):
+#               Angaar Hamid
+#               Andrew Doan
+#               Alejandro Ramos
+#==============================================
+# Last Modified: 12/15/2022
+#==============================================
+# Description:
+# This file is responsible for running the
+# game and creating the objects required
+# to run the game.
+#==============================================
+
+import pygame as pg
+
 from constants import RED, WHITE
-from constants import CELL_SIZE
+from constants import size
 from checker_board import Board
 
-class Game:
-    def __init__(self, win):
-        self._init()
-        self.win = win
-    
-    def update(self):
-        self.board.draw(self.win)
-        self.draw_valid_moves(self.valid_moves)
-        pygame.display.update()
+# Generates and runs the game loop for the checkers game.
+# Calls onto the AI to make moves and displays each move
+# in the window.
 
+class Game:
+    def __init__(self, window):
+        self._init()
+        self.window = window
+
+    # redraws the board onto the screen after each move is made
+    def update(self):
+        self.board.draw(self.window)
+        self.draw_valid_moves(self.valid_moves)
+        pg.display.update()
+
+    # this is used to reset the board when restarting a move
     def _init(self):
         self.selected = None
-        self.board = Board()
         self.turn = RED
         self.valid_moves = {}
+        self.board = Board()
 
-    def winner(self):
-        return self.board.winner()
-
+    # resetting the board after a possible move is displayed to the player
     def reset(self):
         self._init()
 
+    # moves a piece on the board to a specified spot
+    def move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
+            passed = self.valid_moves[(row, col)]
+            
+            if passed:
+                self.board.remove(passed)
+            self.change_turn()
+        else:
+            return False
+
+        return True
+
+    # choose a piece to move
     def select(self, row, col):
         if self.selected:
-            result = self._move(row, col)
+            result = self.move(row, col)
+            
             if not result:
                 self.selected = None
                 self.select(row, col)
@@ -40,24 +81,13 @@ class Game:
             
         return False
 
-    def _move(self, row, col):
-        piece = self.board.get_piece(row, col)
-        if self.selected and piece == 0 and (row, col) in self.valid_moves:
-            self.board.move(self.selected, row, col)
-            skipped = self.valid_moves[(row, col)]
-            if skipped:
-                self.board.remove(skipped)
-            self.change_turn()
-        else:
-            return False
-
-        return True
-
+    # iterates through all of the valid moves generated and displays them one by one onto the screen
     def draw_valid_moves(self, moves):
-        for move in moves:
-            row, col = move
-            pygame.draw.rect(self.win, RED, (col *CELL_SIZE, row *CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        for action in moves:
+            row, col = action
+            pg.draw.rect(self.win, RED, (col *size, row *size, size, size))
 
+    # ends current turn and passed to opponent
     def change_turn(self):
         self.valid_moves = {}
         if self.turn == RED:
@@ -65,9 +95,15 @@ class Game:
         else:
             self.turn = RED
 
+    # only returns the board
     def get_board(self):
         return self.board
 
-    def ai_move(self, board):
-        self.board = board
+    # passes the turn to the AI after opponent has made a turn
+    def ai_move(self, checkers_board):
+        self.board = checkers_board
         self.change_turn()
+
+    # detects whether there is a winner to end the game
+    def winner(self):
+        return self.checkers_board.winner()
