cbde0ec7060e88db2fe2198d8009ac885576d06f
Shmupo <andrewdoan8b@gmail.com>
2022-12-15 20:45:43 -0800

    Changes...

--- checker_board.py
+++ checker_board.py
@@ -1,8 +1,26 @@
+#==============================================
+# Filename: checker_board.py
+#==============================================
+# Filetype: Python Source File
+#==============================================
+# Author(s):
+#               Angaar Hamid
+#               Andrew Doan
+#               Alejandro Ramos
+#==============================================
+# Last Modified: 12/15/2022
+#==============================================
+# Description:
+# This file is responsible for generating,
+# drawing, and running the checker board.
+#==============================================
+
 import pygame
-from constants import NUM_OF_ROWS, NUM_OF_COLUMNS
-from constants import RED, BLACK, WHITE
-from constants import CELL_SIZE
 from checker_piece import CheckerPiece
+
+from constants import NUM_OF_ROWS, NUM_OF_COLUMNS
+from constants import CELL_SIZE as size
+from constants import RED, BLACK, WHITE
 
 class Board:
     def __init__(self):
@@ -10,15 +28,6 @@ class Board:
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
         self.create_board()
-    
-    def draw_squares(self, win):
-        win.fill(WHITE)
-        for row in range(NUM_OF_ROWS):
-            for col in range(row % 2, NUM_OF_COLUMNS, 2):
-                pygame.draw.rect(win, BLACK, (row*CELL_SIZE, col *CELL_SIZE, CELL_SIZE, CELL_SIZE))
-
-    def evaluate(self):
-        return self.white_left - self.red_left + (self.white_kings * 0.5 - self.red_kings * 0.5)
 
     def get_all_pieces(self, color):
         pieces = []
@@ -29,40 +38,25 @@ class Board:
         return pieces
 
     def move(self, piece, row, col):
-        self.board[piece.row_position][piece.column_position], self.board[row][col] = self.board[row][col], self.board[piece.row_position][piece.column_position]
+        col_pos = piece.column_position
+        row_pos = piece.row_position
+        self.board[row_pos][col_pos], self.board[row][col] = self.board[row][col], self.board[row_pos][col_pos]
         piece.move(row, col)
 
         if row == 7 or row == 0:
             piece.make_king()
             if piece.color_of_piece == WHITE:
                 self.white_kings += 1
-            else:
+            elif piece.color_of_piece == RED:
                 self.red_kings += 1 
-
-    def get_piece(self, row, col):
-        return self.board[row][col]
-
-    def create_board(self):
-        for row in range(NUM_OF_ROWS):
-            self.board.append([])
-            for col in range(NUM_OF_COLUMNS):
-                if col % 2 == ((row +  1) % 2):
-                    if row < 3:
-                        self.board[row].append(CheckerPiece(row, col, WHITE))
-                    elif row > 4:
-                        self.board[row].append(CheckerPiece(row, col, RED))
-                    else:
-                        self.board[row].append(0)
-                else:
-                    self.board[row].append(0)
         
-    def draw(self, win):
-        self.draw_squares(win)
+    def draw(self, window):
+        self.draw_squares(window)
         for row in range(NUM_OF_ROWS):
             for col in range(NUM_OF_COLUMNS):
                 piece = self.board[row][col]
                 if piece != 0:
-                    piece.draw(win)
+                    piece.draw(window)
 
     def remove(self, pieces):
         for piece in pieces:
@@ -70,9 +64,79 @@ class Board:
             if piece != 0:
                 if piece.color_of_piece == RED:
                     self.red_left -= 1
-                else:
+                elif piece.color_of_piece == WHITE:
                     self.white_left -= 1
+
+    def create_board(self):
+        for row in range(NUM_OF_ROWS):
+            self.board.append([])
+            
+            for col in range(NUM_OF_COLUMNS):
+                if col % 2 == ( (row +  1) % 2 ):
+                    
+                    if row < 3:
+                        self.board[row].append(CheckerPiece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(CheckerPiece(row, col, RED))
+                    else:
+                        self.board[row].append(0)
+                
+                else:
+                    self.board[row].append(0)
+
+    def traverse_left(self, start, stop, step, color, left, passed=[]):
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if left < 0:
+                break
+            
+            cur = self.board[r][left]
+            if cur == 0:
+                
+                if passed and not last:
+                    break
+                elif passed:
+                    moves[(r, left)] = last + passed
+                else:
+                    moves[(r, left)] = last
+                
+                if last:
+                    
+                    if step == -1:
+                        row = max(r-3, 0)
+                    else:
+                        row = min(r+3, NUM_OF_ROWS)
+                    moves.update(self.traverse_left(r+step, row, step, color, left-1,passed=last))
+                    moves.update(self.traverse_right(r+step, row, step, color, left+1,passed=last))
+
+                break
+            elif cur.color_of_piece == color:
+                break
+            else:
+                last = [cur]
+
+            left -= 1
+        
+        return moves
+
+    def get_valid_moves(self, piece):
+        moves = {}
+        left = piece.column_position - 1
+        right = piece.column_position + 1
+        row = piece.row_position
+        color = piece.color_of_piece
+        
+        if color == RED or piece.is_king:
+            moves.update(self.traverse_left(row -1, max(row-3, -1), -1, color, left))
+            moves.update(self.traverse_right(row -1, max(row-3, -1), -1, color, right))
+        
+        if color == WHITE or piece.is_king:
+            moves.update(self.traverse_left(row +1, min(row+3, NUM_OF_ROWS), 1, color, left))
+            moves.update(self.traverse_right(row +1, min(row+3, NUM_OF_ROWS), 1, color, right))
     
+        return moves
+
     def winner(self):
         if self.red_left <= 0:
             return WHITE
@@ -80,78 +144,38 @@ class Board:
             return RED
         
         return None 
-    
-    def get_valid_moves(self, piece):
-        moves = {}
-        left = piece.column_position - 1
-        right = piece.column_position + 1
-        row = piece.row_position
 
-        if piece.color_of_piece == RED or piece.is_king:
-            moves.update(self._traverse_left(row -1, max(row-3, -1), -1, piece.color_of_piece, left))
-            moves.update(self._traverse_right(row -1, max(row-3, -1), -1, piece.color_of_piece, right))
-        if piece.color_of_piece == WHITE or piece.is_king:
-            moves.update(self._traverse_left(row +1, min(row+3, NUM_OF_ROWS), 1, piece.color_of_piece, left))
-            moves.update(self._traverse_right(row +1, min(row+3, NUM_OF_ROWS), 1, piece.color_of_piece, right))
-    
-        return moves
+    def get_piece(self, row, col):
+        return self.board[row][col]
 
-    def _traverse_left(self, start, stop, step, color, left, skipped=[]):
-        moves = {}
+    def _traverse_right(self, start, stop, step, color, right, passed=[]):
+        valid_moves = {}
         last = []
-        for r in range(start, stop, step):
-            if left < 0:
-                break
-            
-            current = self.board[r][left]
-            if current == 0:
-                if skipped and not last:
-                    break
-                elif skipped:
-                    moves[(r, left)] = last + skipped
-                else:
-                    moves[(r, left)] = last
-                
-                if last:
-                    if step == -1:
-                        row = max(r-3, 0)
-                    else:
-                        row = min(r+3, NUM_OF_ROWS)
-                    moves.update(self._traverse_left(r+step, row, step, color, left-1,skipped=last))
-                    moves.update(self._traverse_right(r+step, row, step, color, left+1,skipped=last))
-                break
-            elif current.color_of_piece == color:
-                break
-            else:
-                last = [current]
-
-            left -= 1
         
-        return moves
-
-    def _traverse_right(self, start, stop, step, color, right, skipped=[]):
-        moves = {}
-        last = []
         for r in range(start, stop, step):
+            
             if right >= NUM_OF_COLUMNS:
                 break
             
             current = self.board[r][right]
+            
             if current == 0:
-                if skipped and not last:
+                if passed and not last:
                     break
-                elif skipped:
-                    moves[(r,right)] = last + skipped
+                elif passed:
+                    valid_moves[(r,right)] = last + passed
                 else:
-                    moves[(r, right)] = last
+                    valid_moves[(r, right)] = last
                 
                 if last:
                     if step == -1:
                         row = max(r-3, 0)
                     else:
                         row = min(r+3, NUM_OF_ROWS)
-                    moves.update(self._traverse_left(r+step, row, step, color, right-1,skipped=last))
-                    moves.update(self._traverse_right(r+step, row, step, color, right+1,skipped=last))
+
+                    valid_moves.update(self.traverse_left(r+step, row, step, color, right-1,passed=last))
+                    valid_moves.update(self.traverse_right(r+step, row, step, color, right+1,passed=last))
+
                 break
             elif current.color_of_piece == color:
                 break
@@ -160,4 +184,13 @@ class Board:
 
             right += 1
         
-        return moves
+        return valid_moves
+
+    def evaluate(self):
+        return self.white_left - self.red_left + (self.white_kings * 0.5 - self.red_kings * 0.5)
+
+    def draw_squares(self, win):
+        win.fill(WHITE)
+        for row in range(NUM_OF_ROWS):
+            for col in range(row % 2, NUM_OF_COLUMNS, 2):
+                pygame.draw.rect(win, BLACK, (row*size, col *size, size, size))
--- checker_piece.py
+++ checker_piece.py
@@ -1,32 +1,51 @@
+#==============================================
+# Filename: checker_piece.py
+#==============================================
+# Filetype: Python Source File
+#==============================================
+# Author(s):
+#               Angaar Hamid
+#               Andrew Doan
+#               Alejandro Ramos
+#==============================================
+# Last Modified: 12/15/2022
+#==============================================
+# Description:
+# This file is responsible for creating checker
+# piece objects on the board.
+#==============================================
+
 import pygame
-from constants import CELL_SIZE, KING_SYMBOL, GREY, OUTER_OUTLINE, INNER_OUTLINE
+
+from constants import CELL_SIZE as size, KING_SYMBOL, GREY, OUTER_OUTLINE as o_line, INNER_OUTLINE as i_line
 
 class CheckerPiece:
 
     def __init__(self, row_position, column_position, color_of_piece):
-        self.row_position = row_position
         self.column_position = column_position
+        self.row_position = row_position
         self.color_of_piece = color_of_piece
-        self.is_king = False
         self.x_position = 0
         self.y_position = 0
-        self.calc_pos()
+        self.king = False
+        self.get_pos()
 
-    def calc_pos(self):
-        self.x_position = CELL_SIZE * self.column_position + CELL_SIZE // 2
-        self.y_position = CELL_SIZE * self.row_position + CELL_SIZE // 2
+    def get_pos(self):
+        self.y_position = (size * self.row_position) + size // 2
+        self.x_position = (size * self.column_position) + size // 2
 
     def make_king(self):
         self.king = True
-    
+
+    def move(self, row_pos, column_pos):
+        self.row_position = row_pos
+        self.column_position = column_pos
+        self.get_pos()
+
     def draw(self, win):
-        radius = CELL_SIZE//2 - OUTER_OUTLINE
-        pygame.draw.circle(win, GREY, (self.x_position, self.y_position), radius + INNER_OUTLINE)
-        pygame.draw.circle(win, self.color_of_piece, (self.x_position, self.y_position), radius)
+        r = size//2 - o_line
+        pygame.draw.circle(win, GREY, (self.x_position, self.y_position), r + i_line)
+        pygame.draw.circle(win, self.color_of_piece, (self.x_position, self.y_position), r)
+        
         if self.is_king:
             win.blit(KING_SYMBOL, (self.x_position - KING_SYMBOL.get_width()//2, self.y_position - KING_SYMBOL.get_height()//2))
-
-    def move(self, row_position, column_position):
-        self.row_position = row_position
-        self.column_position = column_position
-        self.calc_pos()
